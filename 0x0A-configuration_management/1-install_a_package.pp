c360cfc67b08e6a7817051671d3f29bd7830d0f4
ThamiBa <baladithami@gmail.com>
2023-11-24 23:19:11 +0100

    ✨ config_management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,6 @@
-#The code will install flask from pip3
-package { 'python3-pip':
-  ensure => installed,
-}
-
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => '/usr/local/bin:/usr/bin',
-  require => Package['python3-pip'],
+# Install flask from pip3 using Puppet
+package {
+  'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
