854a2f330555ce700931636c82266fcb4bd39948
ThamiBa <baladithami@gmail.com>
2023-11-26 15:13:57 +0100

    ✨ config_management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,14 @@
-# Install flask from pip3 using Puppet
-package {
-  'flask':
+# Install Flask 2.1.0 and update Werkzeug
+package { 'python3':
+  ensure => installed,
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
+
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
 }
