c004112f9aaf2a98cc1bf365dfd223bcabb27150
ThamiBa <baladithami@gmail.com>
2024-01-17 02:09:33 +0100

    👨‍💻 api_is_fun

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
-"""prints the titles of the first 10 hot posts listed for a given subreddit"""
+"""
+this doc for module
+"""
 import requests
+
+headers = {"User-Agent": "MyCustomUserAgent/1.0"}
 
 
 def top_ten(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {'User-Agent': 'Mozilla/5.0 (Linux x86_64) Ama45'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
-        print(None)
-        return
-    data = response.json()['data']['children']
-    for post in data:
-        print(post['data']['title'])
+    """method doc"""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    response = requests.get(url, allow_redirects=False, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        for post in data["data"]["children"]:
+            print(post["data"]["title"])
+    else:
+        print("None")
